56e9d7790e683663ef360b8b941bdc32339ccd0d
Macluster <deepakjudejohn@gmail.com>
2023-04-14 12:35:07 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -63,12 +63,11 @@ class _MyHomePageState extends State<MyHomePage> {
            Text("Click Here", style: TextStyle(color: Colors.blue),)
         ],
       ),
+      SizedBox(height: 50,),
        Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const  [
-          Text("Dont have an acoount",),
-          SizedBox(width: 10,),
-           Text("Click Here", style: TextStyle(color: Colors.blue),)
+        children:  [
+  SignInCard("https://www.google.com/imgres?imgurl=https%3A%2F%2Fassets.stickpng.com%2Fimages%2F5847f9cbcef1014c0b5e48c8.png&tbnid=6iDpAdyA65M1MM&vet=12ahUKEwjk0Pfj5Kj-AhWbFLcAHSCTCpcQMygAegUIARDHAQ..i&imgrefurl=https%3A%2F%2Fwww.stickpng.com%2Fimg%2Ficons-logos-emojis%2Ftech-companies%2Fgoogle-g-logo&docid=v61A3qKPMcJtPM&w=500&h=512&q=google%20transparent%20icon&ved=2ahUKEwjk0Pfj5Kj-AhWbFLcAHSCTCpcQMygAegUIARDHAQ")
         ],
       )
     ],),
@@ -76,4 +75,22 @@ class _MyHomePageState extends State<MyHomePage> {
     ),
     );
   }
+}
+
+
+class SignInCard extends StatelessWidget
+{
+  
+  String imageLink="";
+  SignInCard(this.imageLink);
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+      child: Image.network(imageLink),
+      height: 50,width: 50, decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(25)),color: Colors.white,boxShadow: [BoxShadow(blurRadius: 10,spreadRadius: 10,color: Color.fromARGB(255, 246, 242, 242))]));
+  }
+
 }
